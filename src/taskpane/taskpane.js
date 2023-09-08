50adb9fc0e8fb3e69a33b271517a9e73625a7709
Alec Su <ae40515@yahoo.com.tw>
2023-09-08 22:17:50 +0800

    Initial commit

---
+++ src/taskpane/taskpane.js
@@ -0,0 +1,53 @@
+/*
+ * Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license.
+ * See LICENSE in the project root for license information.
+ */
+
+/* global document, Office, Word */
+
+import hljs from 'highlight.js/lib/core';
+import javascript from 'highlight.js/lib/languages/javascript';
+import computedStyleToInlineStyle from 'computed-style-to-inline-style';
+
+hljs.registerLanguage('javascript', javascript);
+
+Office.onReady((info) => {
+  if (info.host === Office.HostType.Word) {
+    document.getElementById("sideload-msg").style.display = "none";
+    document.getElementById("app-body").style.display = "flex";
+    document.getElementById("insert-code").onclick = () => tryCatch(insertCode)
+  }
+});
+
+/** Default helper for invoking an action and handling errors. */
+async function tryCatch(callback) {
+  try {
+    await callback();
+  } catch (error) {
+    // Note: In a production add-in, you'd want to notify the user through your add-in's UI.
+    console.error(error);
+  }
+}
+
+async function insertCode() {
+  await Word.run(async (context) => {
+    const doc = context.document;
+    const originalRange = doc.getSelection();
+    const code = document.getElementById("code");
+    const highlightedCode = hljs.highlight(
+      code.value,
+      { language: 'javascript' }
+    );
+
+    const result = document.getElementById("result");
+    result.innerHTML = highlightedCode.value;
+    computedStyleToInlineStyle(result, {
+      recursive: true,
+    });
+
+    // It seems that Word doesn't support 'white-space' css
+    originalRange.insertHtml(result.innerHTML.replace(/(?:\r\n|\r|\n)/g, '<br>'), Word.InsertLocation.end);
+
+    await context.sync();
+  })
+}
